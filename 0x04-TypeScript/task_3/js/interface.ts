5e4ab70a656926cf75dc821888a4f87b950339ad
Samueluba <111093750+Samueluba@users.noreply.github.com>
2026-02-03 10:02:18 +0100

    Update interface.ts

--- 0x04-TypeScript/task_3/js/interface.ts
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -5,3 +5,10 @@ export interface RowElement {
   lastName: string;
   age?: number;
 }
+export type RowID = number;
+
+export interface RowElement {
+  firstName: string;
+  lastName: string;
+  age?: number;
+}
